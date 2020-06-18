ec90abe599b8d44ccb7ee50e9785b34a56ccf67d
naylinhtunit <naylin.htun@yathar.com>
2020-06-18 15:12:26 +0630

    Navigation smooth update

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,29 +1,24 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import {useTransition, animated} from 'react-spring'
 
 function Navigation() 
 {
 	const [ showMenu, setShowMenu ] = useState(false)
+	
+	const maskTransitions = useTransition(showMenu, null, {
+		from: { position: 'absolute', opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+	})
+	
+	const menuTransitions = useTransition(showMenu, null, {
+		from: { opacity: 0, transform: 'translateX(-100%)' },
+		enter: { opacity: 1, transform: 'translateX(0%)' },
+		leave: { opacity: 0 , transform: 'translateX(-100%)'},
+	})
 
-	let menu
-	let menuMask
-
-	if(showMenu)
-	{
-		menu = 
-					<div 
-						className="bg-gray-200 fixed top-0 left-0 w-4/5 h-full z-50 shadow"
-					>
-						The menu
-					</div>
-		menuMask =
-						<div
-							className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-							onClick={ () => setShowMenu(false) }
-						>
-						</div>
-	}
 	return (
 			<nav>
 				<span className="text-xl">
@@ -32,9 +27,34 @@ function Navigation()
 						onClick={ () => setShowMenu(!showMenu) }
 					/>
 				</span>
-				{ menuMask }
-				{ menu }
+				{
+					maskTransitions.map(({ item, key, props }) =>
+					item && 
+					<animated.div
+						key={key} 
+						style={props}
+						className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+						onClick={ () => setShowMenu(false) }
+						>
+						</animated.div>
+					)
+				}
 
+				{
+					menuTransitions.map(({ item, key, props }) =>
+					item && 
+					<animated.div
+						key={key} 
+						style={props}
+						className="bg-gray-200 fixed top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+						>
+							<span className="font-bold">Logo</span>
+							<ul>
+								<li>Home</li>
+							</ul>
+						</animated.div>
+					)
+				}
 			</nav>
 		)
 }
