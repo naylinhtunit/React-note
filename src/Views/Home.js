222c2b5b3bd150c1aba390159b8bf95867da4bf1
naylinhtunit <naylin.htun@yathar.com>
2020-06-22 17:36:03 +0630

    Hooks HttpRequests and used useAxiosGet

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,38 +3,11 @@ import axios from 'axios'
 import HelloWorld from '../Components/HelloWorld'
 import Loader from '../Components/Loader'
 import ProductCard from '../Components/ProductCard'
+import {useAxiosGet} from '../Hooks/HttpRequests'
 
 function Home() {
 	const url = `https://5ef02ae0ad6d710016179f45.mockapi.io/api/v1/product?page=1&limit=10`;
-	const [ products, setProducts] = useState({
-		loading: false,
-		data: null,
-		error: false
-	})
-
-	useEffect( () => {
-		setProducts({
-			loading: true,
-			data: null,
-			error: false
-		})
-		axios.get(url)
-					.then(response => {
-						setProducts({
-							loading: false,
-							data: response.data,
-							error: false
-						})
-					})
-					.catch(() => {
-						setProducts({
-							loading: false,
-							data: null,
-							error: true
-						})
-					})
-	}, [url])
-
+	let products = useAxiosGet(url) 
 	let content = null;
 
 	if (products.error) {
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -2,39 +2,15 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {useParams} from 'react-router-dom'
 import Loader from '../Components/Loader'
+import {useAxiosGet} from '../Hooks/HttpRequests'
 
 function Product() {
 	const { id } = useParams()
 	const url = `https://5ef02ae0ad6d710016179f45.mockapi.io/api/v1/product/${id}`
-	const [product, setProduct] = useState({
-		loading: false,
-		data: null,
-		error: false
-	})
-	let content = null
 
-	useEffect( () => {
-		setProduct({
-			loading: true,
-			data: null,
-			error: false
-		})
-		axios.get(url)
-					.then(response => {
-						setProduct({
-							loading: false,
-							data: response.data,
-							error: false
-						})
-					})
-					.catch(() => {
-						setProduct({
-							loading: false,
-							data: null,
-							error: true
-						})
-					})
-	}, [url])
+	let product = useAxiosGet(url)
+	
+	let content = null
 
 	if (product.error) {
 		content = 
